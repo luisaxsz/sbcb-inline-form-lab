b10cb5e543169afffa82c2d21ef985dcfc4706af
luisa lameirao <luisa.lameirao@tre-pa.jus.br>
2025-03-10 16:35:49 -0300

    feat: add row

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,77 @@
+import {FormBuilder, FormGroup, ReactiveFormsModule} from '@angular/forms';
 import {Component} from '@angular/core';
+import {TableModule} from 'carbon-components-angular';
+import {NgForOf, NgIf} from '@angular/common';
+
+export enum FormMode{
+  EDIT,
+  CREATE,
+  DETAIL
+}
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [
+    TableModule,
+    ReactiveFormsModule,
+    NgForOf,
+    NgIf
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'sbcb-inline-form-lab';
+
+  formGroup: FormGroup;
+
+  unidadesDatasource: any[] = [
+    {id: 1, nome: 'Unidade 1'},
+    {id: 2, nome: 'Unidade 2'},
+    {id: 3, nome: 'Unidade 3'},
+    {id: 4, nome: 'Unidade 4'},
+  ]
+
+  cargoDatasource: any[] = [
+    {id: 1, nome: 'Cargo 1'},
+    {id: 2, nome: 'Cargo 2'},
+    {id: 3, nome: 'Cargo 3'},
+    {id: 4, nome: 'Cargo 4'},
+  ]
+
+  vagas: any[] = []
+
+  constructor(
+    private fb: FormBuilder
+  ) {
+
+    this.formGroup = this.fb.group({
+      unidade: [''],
+      cargo: [''],
+      vaga: ['']
+    })
+  }
+
+  addRow() {
+    this.vagas.push(
+      this.fb.group({
+        unidade: [''],
+        cargo: [''],
+        vaga: ['']
+      })
+    );
+  }
+
+  editRow(item: any) {
+    item.backup = { ...item }; // Salva backup antes da edição
+    item.editing = true;
+  }
+
+  saveRow(item: any) {
+    item.editing = false;
+  }
+
+  cancelEdit(item: any) {
+    Object.assign(item, item.backup); // Restaura backup
+    item.editing = false;
+  }
 }
